bf8d45ce4723346edfb49e8bfda5004d80849795
Augusto <augustohansel@hotmail.com>
2022-01-28 15:08:38 -0300

    feat: add post to /messages

--- index.js
+++ index.js
@@ -6,22 +6,23 @@ import joi from 'joi';
 import dayjs from 'dayjs';
 
 const server = express();
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 server.use(express.json());
 server.use(cors());
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+// post participants
 server.post('/participants', async (request, response) => {
+  const participantsSchema = joi.object({
+    name: joi.string().required(),
+  });
+  const validation = participantsSchema.validate(request.body);
+  if (validation.error) {
+    response.sendStatus(422);
+    return;
+  }
   try {
     await mongoClient.connect();
-    const participantsSchema = joi.object({
-      name: joi.string().required(),
-    });
-    const validation = participantsSchema.validate(request.body);
-    if (validation.error) {
-      response.sendStatus(422);
-      return;
-    }
     const allParticipants = await mongoClient.db('batePapoUol').collection('participants').find({}).toArray();
     const alreadyHaveParticipant = allParticipants.find(
       (participant) => participant.name === request.body.name,
@@ -46,25 +47,68 @@ server.post('/participants', async (request, response) => {
 
     response.sendStatus(201);
   } catch {
-    response.status(422).send('erro');
+    response.sendStatus(500);
   }
 });
+
+// get participants
 server.get('/participants', async (request, response) => {
   try {
     await mongoClient.connect();
     const participants = await mongoClient.db('batePapoUol').collection('participants').find({}).toArray();
     response.send(participants);
   } catch {
-    response.status(422).send('erro');
+    response.sendStatus(500);
   }
 });
-server.post('/messages', (request, response) => {
 
-});
-server.get('/messages', (request, response) => {
+// post message
+server.post('/messages', async (request, response) => {
+  const messagesSchema = joi.object({
+    from: joi.string().required(),
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('private_message', 'message').required(),
+    time: joi.string().required(),
+  });
 
+  const completeMessage = {
+    ...request.body,
+    from: request.headers.user,
+    time: dayjs().format('HH:mm:ss'),
+  };
+  const validation = messagesSchema.validate(completeMessage);
+  if (validation.error) {
+    response.sendStatus(422);
+    return;
+  }
+  try {
+    await mongoClient.connect();
+    const activeUser = await mongoClient.db('batePapoUol').collection('participants').findOne({ name: request.headers.user });
+    if (!activeUser) {
+      response.sendStatus(422);
+    }
+    await mongoClient.db('batePapoUol').collection('messages').insertOne(completeMessage);
+    response.sendStatus(201);
+  } catch (error) {
+    response.sendStatus(500);
+  }
 });
-server.post('/status', (request, response) => {
+
+// get message
+server.get('/messages', async (request, response) => {
+  try {
+    await mongoClient.connect();
+    const messages = await mongoClient.db('batePapoUol').collection('messages').find({}).toArray();
+    if (!request.query.limit) {
+      response.send(messages);
+    }
+    response.send(messages);
+  } catch {
+    response.sendStatus(422);
+  }
+});
+server.post('/status', async (request, response) => {
 
 });
 
